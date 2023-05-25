c1b8f441c87d5ccb228853634f62e58cfdde9537
Beatriz Neto <ist1103149@tecnico.ulisboa.pt>
2023-05-25 21:23:35 +0100

    25_05

---
+++ intro/Hist_Lab0_LFEA.cpp
@@ -0,0 +1,59 @@
+
+void Hist_Lab0_LFEA() {
+
+    TApplication App("A", nullptr, nullptr);
+    TCanvas C("C","",1200,800);
+
+    TH1F* histogram = new TH1F("histogram", "Uniformly Distributed Bins", 300, 0, 1023);
+    TF1 *gaussian = new TF1("gaussian", "gaus", 420, 500);
+
+    std::vector<double> xData;
+    std::vector<double> yData;
+
+    std::ifstream file("cesio_sem_ruido.txt");
+    if (!file.is_open()) {
+        std::cerr << "Error opening file: " << "COM.FONTE.txt" << std::endl;
+        return;
+    }
+    
+    std::string line;
+    while (std::getline(file, line)) {
+        double x, y;
+        char comma;
+        std::stringstream ss(line);
+        ss >> x >> comma >> y;
+        xData.push_back(x);
+        yData.push_back(y);
+    }
+    
+    file.close();
+
+    //Adding points to graph
+    for (int i=0; i<xData.size(); i++) {
+
+        histogram->Fill(xData[i], yData[i]);
+
+    }
+
+    double min = 420;
+    double max = 500;
+
+    histogram->GetXaxis()->SetRangeUser(min, max);
+    histogram->Fit(gaussian);
+
+
+    histogram->SetTitle("Amplitude dos Pulsos");
+    histogram->SetMarkerColor(kBlue-2);
+    histogram->GetXaxis()->SetTitle("Amplitude");
+    histogram->GetYaxis()->SetTitle("N de Contagens");
+    histogram->Draw("HIST");
+
+    //TF1* fitFunc = new TF1("fitFunc", "gaus", 0, 10);
+    //TFitResultPtr fitResult = histogram->Fit(fitFunc, "S");
+    //fitFunc->Draw("same");
+    gaussian->Draw("same");
+    C.SaveAs("pioripipi.png");
+    C.Update();
+    C.WaitPrimitive();
+
+}
---
+++ intro/log_ruido.cpp
@@ -0,0 +1,123 @@
+
+void log_ruido() {
+
+    TApplication App("A", nullptr, nullptr);
+    TCanvas C("C","",1200,800);
+
+    TH1F* histogram = new TH1F("histogram", "Uniformly Distributed Bins", 100, 0, 1023);
+    TH1F* histograma = new TH1F("histograma", "Uniformly Distributed Bins", 100, 0, 1023);
+    TH1F* subtracao = new TH1F("histograma", "Uniformly Distributed Bins", 100, 0, 1023);
+
+    std::vector<double> xData;
+    std::vector<double> yData;
+    std::vector<double> xdata;
+    std::vector<double> ydata;
+    std::vector<double> sub;
+    double valor_sub;
+
+    std::ifstream file("RUIDO.CESIO.txt");
+    if (!file.is_open()) {
+        std::cerr << "Error opening file: " << "RUIDO.CESIO.txt" << std::endl;
+        return;
+    }
+    
+    std::string line;
+    while (std::getline(file, line)) {
+        double x, y;
+        char comma;
+        std::stringstream ss(line);
+        ss >> x >> comma >> y;
+        xData.push_back(x);
+        yData.push_back(y);
+    }
+    
+    file.close();
+
+    std::ifstream files("COM.FONTE.txt");
+    if (!files.is_open()) {
+        std::cerr << "Error opening file: " << "COM.FONTE.txt" << std::endl;
+        return;
+    }
+    
+    while (std::getline(files, line)) {
+        double a, b;
+        char co;
+        std::stringstream ss(line);
+        ss >> a >> co >> b;
+        xdata.push_back(a);
+        ydata.push_back(b);
+    }
+
+    
+    
+    files.close();
+
+    //Adding points to graph
+    for (int i=0; i<xData.size(); i++) {
+
+        histogram->Fill(xData[i], yData[i]);
+
+    }
+
+    //Adding points to graph
+    for (int i=0; i<xdata.size(); i++) {
+
+        histograma->Fill(xData[i], ydata[i]);
+
+    }
+
+    //Creating points for subtraction
+    for (int i=0; i<xdata.size(); i++) {
+        valor_sub=ydata[i]-yData[i];
+        sub.push_back(valor_sub);
+        //cout<< sub[i] << endl;
+
+    }
+
+    //Adding points to graph
+    for (int i=0; i<xdata.size(); i++) {
+        subtracao->Fill(xData[i], sub[i]);
+
+    }
+
+    histogram->SetStats(0);
+    histograma->SetStats(0);
+    subtracao->SetStats(0);
+
+    //criar legendas para os gráficos
+    TLegend *legend = new TLegend(0.7, 0.7, 0.9, 0.9);  // (x1, y1, x2, y2)
+
+
+    histograma->SetTitle("Erro sistematico - Radiacao ambiente");
+    histograma->SetLineColor(kRed);
+    histogram->SetLineColor(kGreen);
+    histogram->SetLineWidth(4);
+    histograma->SetLineWidth(4);
+    histograma->GetXaxis()->SetTitle("Bins");
+    histograma->GetYaxis()->SetTitle("N de Contagens");
+    histogram-> SetFillColor(kGreen);
+    histograma-> SetFillColor(kRed);
+    
+    C.SetLogy();
+
+    histograma->Draw("Hist");
+    histogram->Draw("SAME HIST");
+    subtracao->SetLineColor(kBlue-2);
+    subtracao->SetLineWidth(4);
+    subtracao->Draw("SAME HIST");
+
+    legend->AddEntry(histogram, "Contagens sem fonte", "l");  // (object, label, option)
+    legend->AddEntry(histograma, "Contagens com fonte", "l");  // (object, label, option)
+    legend->AddEntry(subtracao, "Subtracao (com fonte-sem fonte)", "l");  // (object, label, option)
+    legend->Draw();
+
+    /*TF1* fitFunc = new TF1("fitFunc", "gaus", 0, 10);
+    TFitResultPtr fitResult = histogram->Fit(fitFunc, "S");
+    fitFunc->Draw("same");*/
+
+    C.SaveAs("log_ruido.png");
+
+    C.Update();
+    C.WaitPrimitive();
+
+}

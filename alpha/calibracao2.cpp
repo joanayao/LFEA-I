c5d9da97b945b9f34c7807f52f90c0c2a4da6eb5
Beatriz Neto <ist1103149@tecnico.ulisboa.pt>
2023-06-04 16:01:25 +0100

    SENDING YOU LOVE BESTIES

--- alpha/calibracao2.cpp
+++ alpha/calibracao2.cpp
@@ -8,15 +8,14 @@ Double_t fitFunction(Double_t* x, Double_t* par) {
     return par[0]*x[0]+par[1];
 }
 
-void fit() {
+void calibracao2() {
     // Step 1: Set up your data points
     //Falta aqui um dos pontos, o de 80 keV
-    Double_t x[] = {677.02,496.17,1011.69,775.42,223.51,23.98,101.22};
-    Double_t y[] = {5.48556,5.02,6.39,5.76,4.29,3.77,3.98};
-    Double_t ex[] = {0.05,0.12,0.14,0.14,0.13,0.15,0.15};
-    Double_t ey[] = {0,0.07,0.08,0.08,0.06,0.06,0.06};
-    Int_t nPoints = 7;
-    double npars=2;
+    Double_t x[] = {702.87,601.61,534.91,943.59,806.7,399.82};
+    Double_t y[] = {5.48556,4.71,4.19,7.33,6.28,3.14};
+    Double_t ex[] = {0.06,0.09,0.09,0.11,0.11,0.11};
+    Double_t ey[] = {0.00,0.07,0.06,0.09,0.08,0.05};
+    Int_t nPoints = 6;
 
     // Step 2: Create a ROOT graph
     TGraphErrors* graph = new TGraphErrors(nPoints, x, y, ex, ey);
@@ -50,7 +49,7 @@ void fit() {
 
     graph -> SetTitle("Calibracao;Bins;Energy(MeV)"); //Title, X title, Y title
     graph->Draw("AP");
-    canvas->SaveAs("calibracao1_nova_am.png");
+    canvas->SaveAs("calibracao2_nova_am.png");
     
     // Display the fit parameters
     std::cout << "Fit Parameters:" << std::endl;
@@ -61,9 +60,7 @@ void fit() {
 
     // Display the chi-square value
     std::cout <<   "Chi-square: " << chi2 << std::endl;
-    std::cout << "numero parametros : " << fitFunc->GetNpar() << std::endl;
     std::cout << "chi/ndf : " << chi2/(nPoints-fitFunc->GetNpar()) << std::endl;
     std::cout << "teste" << graph->GetErrorX(0) << std::endl;
-
 }
 
